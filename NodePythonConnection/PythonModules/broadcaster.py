7b069bbd70f49d80dc98843f0c3aa9618b96dfff
David Jonas <davidjonasdesign@gmail.com>
2014-01-25 02:56:59 +0100

    Created wrapper for SocketIO communication.

--- NodePythonConnection/PythonModules/broadcaster.py
+++ NodePythonConnection/PythonModules/broadcaster.py
@@ -1,1 +1,25 @@
-__author__ = 'davidjonasdesign'
+__author__ = 'David Jonas'
+
+from socketIO_client import SocketIO
+import sys
+
+
+class Broadcaster(object):
+    port = 8080
+    host = "localhost"
+
+    def __init__(self, port=8080, host="localhost"):
+        self.port = port
+        self.host = host
+        self.socketIO = SocketIO(host, port)
+
+        self.socketIO.on("ack", self.logACK)
+
+    def logAck(self, data):
+        print("Acknowledgement received for %s" % data['original'])
+
+    def emit(self, event, data):
+        self.socketIO.emit(event, data)
+
+    def on(self, event, callback):
+        self.socketIO.on(event, callback)
