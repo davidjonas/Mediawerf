1ba505d1d64b209bc4650c204f8e789f9004b72f
David Jonas <davidjonasdesign@gmail.com>
2014-12-11 18:19:45 +0100

    Deugging remote wind

--- NodePythonConnection/serverTest01.js
+++ NodePythonConnection/serverTest01.js
@@ -1,5 +1,5 @@
 var server = require('http').createServer(),
-    io = require('socket.io')(server),
+    io = require('socket.io').listen(server),
     process = require('process');
 
 //port to listen can be set through command line argument by running 'node server.js [port]' (it defaults to 7080)
